100a48f28e4c995425aaa993b182bc187d5244ed
Sameer321 <asyedsameer31@gmail.com>
2026-03-05 10:01:15 +0530

    add socket.io to server

--- webrtc-firebase-demo-main/backend/temp.js
+++ webrtc-firebase-demo-main/backend/temp.js
@@ -1,10 +1,20 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import { createServer } from "http";
+import { Server } from "socket.io";
 
 dotenv.config();
 
 const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
+
 app.use(cors());
 app.use(express.json());
 
@@ -12,6 +22,14 @@ app.get("/", (req, res) => {
   res.send("Backend working");
 });
 
-app.listen(3000, () => {
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+httpServer.listen(3000, () => {
   console.log("Server running on 3000");
 });
